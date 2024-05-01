f3b1a7a443c2a6a2727e0f67239dec3e1ba7e120
noah-aoki-mt <noa.aoki@metateam.co.jp>
2024-05-01 15:33:54 +0900

    場合に応じた処理2

---
+++ Practice2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner
+
+public class Main{
+    public static void main (String[]args){
+        Scanner test = new Scanner(System.in);
+        int num = test.nextInt();
+        swirch(num){
+            case 1:
+            System.ou,println("もっと頑張りましょう");
+            break;
+            case 2:
+            System.ou,println("もう少し頑張りましょう");
+            break;
+            case 3:
+            System.ou,println("さらに上を目指しましょう");
+            break;
+            case 4:
+            System.ou,println("大変よくできました");
+            break;
+            case 4:
+            System.ou,println("大変優秀です");
+            break;
+            case 5:
+            System.ou,println("大変優秀です");
+            break;
+            default;
+            System.ou,println("諦めたらそこで試合終了ですよ");
+ 
+        }
+    }
+}
